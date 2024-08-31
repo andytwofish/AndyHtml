4aeed86888ab5fd663ca31d63c3f48d129f50e90
befoo_lin <befoo.lin@gmail.com>
2024-08-31 10:00:43 +0800

    .

--- bee/EnemyBullet.js
+++ bee/EnemyBullet.js
@@ -73,36 +73,54 @@ class EnemyBullet extends Entity {
 }
 
 class Missile extends EnemyBullet{
-    run = 64 ;
     direction = 0 ;
-    target = target[Math.floor(Math.random()*MyTank.objs.length)] ;
+
+    init() {
+        let part0 = new Part() ;
+        part0.add(0,0) ;
+        part0.fillStyle = "rgb(200,0,0)" ;
+
+        this.parts = [] ;
+        this.parts.push( part0 ) ;
+        this.target = this.findTarget() ;
+    }
+
+    findTarget() {
+        if ( MyTank.objs.length <= 0 ) {
+            return null;
+        }
+        return MyTank.objs[Math.floor(Math.random()*MyTank.objs.length)] ;
+    }
+
     process(){
-        if (this.run-- < 0 ){
-            this.hp = 0 ;
-            return ;
+        if ( this.target.hp != null && this.target.hp <= 0 ) {
+            this.target = this.findTarget() ;
+            if ( this.target == null ) {
+                return ;
+            }
         }
         if (Math.floor(Math.random()*2) == 0){
             if (this.row>this.target.row){
-                this.row-- ;
-                this.direction = 0 ;
+                this.moveDirection=0;
+                this.direction=0;
             }else{
                 if(this.row<this.target.row){
-                    this.row++ ;
-                    this.direction = 180 ;
+                    this.moveDirection=180;
+                    this.direction=180;
                 }
             }
         }else{
             if (this.col>this.target.col){
-                    this.col-- ;        
-                    this.direction = 270 ;                           
+                    this.moveDirection=270;
+                    this.direction=270;
             }else{
                 if(this.col<this.target.col){
-                    this.col++ ;
-                    this.direction = 90 ;
+                    this.moveDirection=90;
+                    this.direction=90;
                 }                                          
             }
         }
-        
+        this.move() ;        
         this.draw() ;  
     }
     draw(){
@@ -112,16 +130,35 @@ class Missile extends EnemyBullet{
     }
 }
 class LaserLight extends EnemyBullet{
-    hp = 7 ;
-    process(){
-        this.hp-- ;
-        if ( this.hp<1 ) {
-            return ;
-        }
-        this.draw()          
+    moveDistance = 100 ;
+    moveDirection = 0 ;
+    init() {
+        let part0 = new Part() ;
+        part0.add(0,0).add(-1,0).add(-2,0).add(-3,0).add(-4,0).add(-5,0).add(-6,0) ;
+        part0.fillStyle = "rgb(200,0,0)" ;
+        part0.hp = 999 ;
+
+        this.parts = [] ;
+        this.parts.push( part0 ) ;
     }
+
+
+   move() {
+        if ( this.moveDistance-- > 0 )  {
+            let isMove = this.moveOutBoundary( this.row+6, this.col ) ;
+            if ( isMove ) {
+                this.checkAfterMoved() ;
+            } else {
+                this.hp = 0 ;
+            }
+
+        } else {
+            this.hp = 0 ;
+        }
+
+    }
+
     draw(){
-        ctx.fillStyle = `rgb(100,0,0)` ;
-        ctx.fillRect( this.col*CELL_SIZE, this.row*CELL_SIZE, CELL_SIZE, CELL_SIZE ) ;
+        this.autoDraw() ;
     }
 }
