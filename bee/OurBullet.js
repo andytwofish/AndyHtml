a39b694dc3045272f87d229e46196b3ff8390589
AndyTwoFish <twoyu_lin@gmail.com>
2024-09-22 09:37:17 +0800

    ok

--- bee/OurBullet.js
+++ bee/OurBullet.js
@@ -105,7 +105,7 @@ class OurBomb extends OurBullet {
 
         let part = new Part() ;
         part.fillStyle = "rgb(200,0,0)" ;
-        part.hp=1000;
+        part.hp=this.bombLevel*5;
         for( let i=0; i<this.bombLevel; i++ ) {
             for( let j=-i; j<=i; j++) {
                 part.add( i-this.bombLevel, j ) ;
