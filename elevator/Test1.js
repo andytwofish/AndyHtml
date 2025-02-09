21c4c0899967af91402344fa4a55733ed56a6802
befoo_lin <befoo.lin@gmail.com>
2025-02-09 19:33:52 +0800

    add Elevator Test1

---
+++ elevator/Test1.js
@@ -0,0 +1,64 @@
+class UserKey {
+    toFloor = 0 ;
+    condition = CONDITION_ANY ;
+    constructor( toFloor, condition ){
+        this.toFloor = toFloor ;
+        this.condition = condition ;
+    }
+}
+
+class Test1 {
+    tasks = [] ;
+    index=-1;
+    beginTime = 0 ;
+    elevatorMoveTime = 2000 ;
+    lastMoveTime = 0 ;
+    elevator = new Elevator(10) ;
+    constructor(){
+        this.addTask(5, 1, CONDITION_UP ) ;
+        this.addTask(6, 8, CONDITION_ANY ) ;
+        this.addTask(7, 5, CONDITION_UP ) ;
+        this.addTask(8, 10, CONDITION_ANY ) ;
+    }
+    addTask( seconds, toFloor, condition ) {
+        let obj = new Object() ;
+        obj.ms = seconds*1000 ;
+        obj.userKey = new UserKey( toFloor, condition ) ;
+        this.tasks.push(obj) ;
+    }
+    start() {
+        console.log(this.tasks) ;
+        this.beginTime = Date.now() ;
+        this.index=0;
+    }
+    next() {
+        if ( Date.now() - this.lastMoveTime > this.elevatorMoveTime ) {
+            this.lastMoveTime = Date.now() ;
+            this.elevator.next();
+        }
+        if ( this.index == -1 ) {
+            return ;
+        }
+        if ( Date.now()-this.beginTime >= this.tasks[this.index].ms ) {
+            let userKey = this.tasks[this.index].userKey ;
+            //console.log(userKey) ;
+            switch( userKey.condition ) {
+                case CONDITION_ANY:
+                    this.elevator.goTo( userKey.toFloor ) ;
+                    break;
+                case CONDITION_UP:
+                    this.elevator.up( userKey.toFloor ) ;
+                    break;
+                case CONDITION_DOWN:
+                    this.elevator.down( userKey.toFloor ) ;
+                    break;
+            }
+            if ( ++this.index >= this.tasks.length ) {
+                this.index = -1 ;
+                return ;
+            }
+            this.next() ;
+        }
+    }
+}
+
--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,23 +1,37 @@
+class Task {
+    toFloor = 0 ;
+    condition = CONDITION_ANY ;
+}
+
 class Elevator {
     numberOfFloors = 0 ;
     whereIsElevator = 0 ;
+    tasks = [] ;
     constructor(numberOfFloors){
         this.numberOfFloors = numberOfFloors ;
     }
     next() {
-        console.log("Elevator next()");
-        console.log(this.numberOfFloors);
+        console.log(this.tasks) ;
+        //openEvent(elevatorId) ;
+
     }
     up(fromFloor) {
-    
+        let task = new Task() ;
+        task.condition = CONDITION_UP ;
+        task.toFloor = fromFloor ;
+        this.tasks.push(task) ;
     }
     down(fromFloor) {
-    
+        let task = new Task() ;
+        task.condition = CONDITION_DOWN ;
+        task.toFloor = fromFloor ;
+        this.tasks.push(task) ;
     }
-    goTo(elevatorId,toFloor) {
-    
+    goTo(toFloor) {
+        let task = new Task() ;
+        task.condition = CONDITION_ANY ;
+        task.toFloor = toFloor ;
+        this.tasks.push(task) ;
     }
-    //openEvent(elevatorId) ;
 }
-    
 
